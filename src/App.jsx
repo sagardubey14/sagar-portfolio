2617a5ef0081cf0d1a41e3346c0f712829dbde5f
sagardubey14 <sagardubey353@gmail.com>
2025-05-22 02:57:24 +0530

    Updates regarding Skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import AboutMe from "./Components/AboutMe";
 import Home from "./Components/Home";
 import Education from "./Components/Education";
 import SkillsSection from "./Components/SkillsSection";
-import ImageCarousel from "./Components/ImageCarousel";
-import ProjectSection from "./Components/ProjectSection";
-
 function App() {
 
   return (
@@ -13,7 +10,6 @@ function App() {
       <AboutMe />
       <Education />
       <SkillsSection />
-      <ProjectSection />
     </>
   );
 }
